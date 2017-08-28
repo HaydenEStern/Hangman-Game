6030eff2326870537c102670d47dda0d8d9be510
Hayden Stern <haydenstern@Haydens-MBP.att.net>
2017-08-27 17:07:59 -0700

    add more code to javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,106 @@
-// declare all variables that will be used inside the function
-var words = ["Terezi", "Davesprite", "Karkat", "Vriska", "Ferferi", "Rose"]
-var guessCounter = 12;
-var winCounter = 0;
-var wordBlank;
-var activeWord;
-var splitWord = [];
+document.onclick = function(event) {
 
-// select a random word with which to start
-var activeWord = words[Math.floor(Math.random() * words.length)];
-var splitWord = activeWord.split("");
-var wordBlank = splitWord.fill("_ ").join("");
-// display the (hidden) first word when any key is pressed
+    var hangman = {
+        guessCounter: 12,
+        winCounter: 0,
+        words: ["terezi", "davesprite", "karkat", "vriska", "ferferi", "rose"],
+        activeWord: "",
+        splitWord: [],
+        wordBlank: [],
+        userInput: "",
+        unBlank: [],
+        // randomly selects a word from the words array
+        pickWords: function() {
+            this.activeWord = this.words[Math.floor(Math.random() * this.words.length)];
+        },
+        // splits the string into an array with one letter per index and stores it as a new key
+        splitWords: function() {
+            this.splitWord = this.activeWord.split("");
+        },
+        // copies the split array and replaces its contents with the "_" character and
+        // then rejoins it into a string
+        makeBlanks: function() {
+            this.wordBlank = this.splitWord.slice().fill("_ ").join("");
 
-document.onkeyup = function(event) {
+        },
 
-document.getElementById("game-text").innerHTML = wordBlank;
+
+
+
+
+        // picks a word, splits it into an array, and displays the blank spaces on the screen
+        startGame: function() {
+            this.pickWords();
+            this.splitWords();
+            this.makeBlanks();
+
+        },
+
+        splitBlank: function() {
+            this.unBlank = this.wordBlank.split(" ");
+        },
+
+        replaceBlank: function() {
+        	for (i = 0; i < this.splitWord.length; i++) {
+            if (this.splitWord[i] === this.userInput) {
+                
+               this.unBlank[i] = this.userInput;
+               this.unBlank.pop();
+               this.unBlank = this.unBlank.join(" ");
+               return this.unBlank;
+
+            }
+            else {
+            	console.log("no match");
+            };
+        	};
+    	},
+        
+
+       // iterateBlank: function() {
+
+         //   this.unBlank = this.splitWord.forEach(this.replaceBlank);
+
+       // },
+
+        //	this.unBlank = this.wordBlank.split("").splice(i, i, this.userInput);
+
+        // check if userInput matches a character in splitWords
+        //        checkLetter: function() {
+        //        	for (i = 0; i < this.splitWord.length; i++) {
+        //       		if (this.userInput === this.splitWord[i]) {
+        // split blankWord and replace blank at index [i] with userInput
+        // and then rejoin
+        //					this.fillBlanks();
+        //				console.log(this.wordBlank);
+        //  		};
+        //	};
+        // },
+
+
+    };
+    hangman.startGame();
+    document.getElementById("game-text").innerHTML = hangman["wordBlank"];
+    // stores key pressed by user as the value of the userInput key inside the hangman object
+    document.onkeyup = function(event) {
+        hangman["userInput"] = event.key;
+        //     var checkLetter = function() {
+
+        hangman.splitBlank();
+        hangman.replaceBlank();
+        document.getElementById("game-text").innerHTML = hangman["unBlank"];
+        //    		hangman.splitWord.forEach(function(letter, index, unBlank) {  
+        //  			if (hangman.userInput === hangman.splitWord[i]) {   		
+        //			hangman.unBlank[index] = this.userInput;
+    };
+    //	});
+    //   for (i = 0; i < hangman.splitWord.length; i++) {
+    //      
+    // split blankWord and replace blank at index [i] with userInput
+    // and then rejoin
 };
-//store user input as a variable
-/*	document.onkeyup = function(event) {
-		userInput = event.key;
-//search array of letters for for userInput
-		for (i = 0; i < splitWord.length; i++) {
-//replace blank space with letter			
-			if (splitWord[i] = userInput) {
-				console.log(userInput);
-			};
 
-		};
-	};
 
-*/
+
+
+
